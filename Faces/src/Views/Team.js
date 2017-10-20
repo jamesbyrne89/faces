eed57dd2c87b89a6cddcc014d912ac1f3eb15163
jamesbyrne89 <mail@jamestbyrne.com>
2017-10-20 22:43:24 +0100

    Moved header to side and added views folder

--- Faces/src/Views/Team.js
+++ Faces/src/Views/Team.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 import './styles.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            profiles: []
-        }
-    }
+const Profile = () => {
     render() {
         return ( < div className = "app-container" >
             <
