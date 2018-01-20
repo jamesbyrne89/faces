ff342f44edd1279f3b20c8ef8f9d7207ec50ecd1
jamesbyrne89 <mail@jamestbyrne.com>
2018-01-20 11:18:14 +0000

    Restructured data object

--- faces/src/App.js
+++ faces/src/App.js
@@ -14,147 +14,167 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            profiles: [{
-                name: "James Byrne",
-                title: "Junior Web Developer",
-                email: "james.byrne@warehouse.co.uk",
-                team: "Digital",
-                photo: "http://via.placeholder.com/250x250"
-            },
-            {
-                name: "James Byrne",
-                title: "Junior Web Developer",
-                email: "james.byrne@warehouse.co.uk",
-                team: "Digital",
-                photo: "http://via.placeholder.com/250x250"
-            },
-            {
-                name: "James Byrne",
-                title: "Junior Web Developer",
-                email: "james.byrne@warehouse.co.uk",
-                team: "Digital",
-                photo: "http://via.placeholder.com/250x250"
-            },
-            {
-                name: "James Byrne",
-                title: "Junior Web Developer",
-                email: "james.byrne@warehouse.co.uk",
-                team: "Digital",
-                photo: "http://via.placeholder.com/250x250"
-            },
-            {
-                name: "James Byrne",
-                title: "Junior Web Developer",
-                email: "james.byrne@warehouse.co.uk",
-                team: "Digital",
-                photo: "http://via.placeholder.com/250x250"
-            },
-            {
-                name: "James Byrne",
-                title: "Junior Web Developer",
-                email: "james.byrne@warehouse.co.uk",
-                team: "Digital",
-                photo: "http://via.placeholder.com/250x250"
-            },
-            {
-                name: "James Byrne",
-                title: "Junior Web Developer",
-                email: "james.byrne@warehouse.co.uk",
-                team: "Digital",
-                photo: "http://via.placeholder.com/250x250"
-            },
-            {
-                name: "Jotis Moore",
-                title: "Lead Developer",
-                email: "jotis.moore@warehouse.co.uk",
-                team: "Digital",
-                photo: "http://via.placeholder.com/250x250"
-            },
-            {
-                name: "Alix Burn",
-                title: "Head Of Digital",
-                email: "alixansDra.burn@warehouse.co.uk",
-                team: "Digital",
-                photo: "http://via.placeholder.com/250x250"
-            }],
             teams: [
-                "Digital",
-                "Brand Comms",
-                "Retail",
-                "Buying"
+                { name: "Digital",
+                    profiles: [{
+                        name: "James Byrne",
+                        title: "Junior Web Developer",
+                        email: "james.byrne@warehouse.co.uk",
+                        team: "Digital",
+                        photo: "http://via.placeholder.com/250x250"
+                    },
+                    {
+                        name: "James Byrne",
+                        title: "Junior Web Developer",
+                        email: "james.byrne@warehouse.co.uk",
+                        team: "Digital",
+                        photo: "http://via.placeholder.com/250x250"
+                    },
+                    {
+                        name: "James Byrne",
+                        title: "Junior Web Developer",
+                        email: "james.byrne@warehouse.co.uk",
+                        team: "Digital",
+                        photo: "http://via.placeholder.com/250x250"
+                    },
+                    {
+                        name: "James Byrne",
+                        title: "Junior Web Developer",
+                        email: "james.byrne@warehouse.co.uk",
+                        team: "Digital",
+                        photo: "http://via.placeholder.com/250x250"
+                    },
+                    {
+                        name: "James Byrne",
+                        title: "Junior Web Developer",
+                        email: "james.byrne@warehouse.co.uk",
+                        team: "Digital",
+                        photo: "http://via.placeholder.com/250x250"
+                    },
+                    {
+                        name: "James Byrne",
+                        title: "Junior Web Developer",
+                        email: "james.byrne@warehouse.co.uk",
+                        team: "Digital",
+                        photo: "http://via.placeholder.com/250x250"
+                    },
+                    {
+                        name: "James Byrne",
+                        title: "Junior Web Developer",
+                        email: "james.byrne@warehouse.co.uk",
+                        team: "Digital",
+                        photo: "http://via.placeholder.com/250x250"
+                    },
+                    {
+                        name: "Jotis Moore",
+                        title: "Lead Developer",
+                        email: "jotis.moore@warehouse.co.uk",
+                        team: "Digital",
+                        photo: "http://via.placeholder.com/250x250"
+                    },
+                    {
+                        name: "Alix Burn",
+                        title: "Head Of Digital",
+                        email: "alixansDra.burn@warehouse.co.uk",
+                        team: "Digital",
+                        photo: "http://via.placeholder.com/250x250"
+                    }]
+                },
+                {
+                    name: "Brand Comms",
+                    profiles: [{
+                        name: "Jill Gate",
+                        title: "Brand Comms Director",
+                        email: "jill.gate@warehouse.co.uk",
+                        team: "Brand Comms",
+                        photo: "http://via.placeholder.com/250x250"
+                    }]
+                },
+                {
+                    name: "Retail",
+                    profiles: []
+                },
+                {
+                    name: "Buying",
+                    profiles: []
+                }
             ],
-            locations: {
-                list: [
-                    "London",
-                    "Stanton Harcourt",
-                    "International",
-                    "International 2",
-                    "International 3"
+        locations: {
+            list: [
+                "London",
+                "Stanton Harcourt",
+                "International",
+                "International 2",
+                "International 3"
                 ],
                 current: "London"
-            },
-            currentLocation: "London",
+        },
             modal: {
-                open: false,
+            open: false,
                 content: null
-            }
         }
     }
+}
 
-    filterTeams(name) {
-        const { profiles } = this.state;
-        let teamName = name.toLowerCase();
-        return profiles.filter(profile => (profile.team).toLowerCase() === teamName);
-    }
+filterTeams(name) {
+    let { teams } = this.state;
+    let teamName = name.toLowerCase();
+    return teams.filter(team => (team.name).toLowerCase() === teamName);
+}
 
-    modalHandler(openState, content) {
-        this.setState({
-            modal: {
-                open: openState,
-                content: content
-            }
-        });
-    }
+getAllEmployees() {
+    let { teams } = this.state;
+  return 
+}
+
+modalHandler(openState, content) {
+    this.setState({
+        modal: {
+            open: openState,
+            content: content
+        }
+    });
+}
 
 
 
 
 
-    render() {
-        const { profiles, teams, locations } = this.state;
-        const { modal } = this.state;
-        const modalHandler = this.modalHandler.bind(this);
-        const filterTeams = this.filterTeams.bind(this);
+render() {
+    const { teams, locations, modal } = this.state;
+    const allEmployees = teams.map(team => team.profiles)
+    const modalHandler = this.modalHandler.bind(this);
+    const filterTeams = this.filterTeams.bind(this);
 
-        return (
-            <BrowserRouter>
-                <div className={"app-container" + (modal.open ? ' modal-opened' : '')} >
-                    <Header />
-                    <main className="app-content">
-                        <header className="controls-bar">
+    return (
+        <BrowserRouter>
+            <div className={"app-container" + (modal.open ? ' modal-opened' : '')} >
+                <Header />
+                <main className="app-content">
+                    <header className="controls-bar">
 
-                            <ul>
-                                <li>Warehouse Fashions</li>
-                                <LocationsDropdown locations={locations} />
-                                <li><button className="btn">Sign Out</button></li>
-                            </ul>
-                        </header>
+                        <ul>
+                            <li>Warehouse Fashions</li>
+                            <LocationsDropdown locations={locations} />
+                            <li><button className="btn">Sign Out</button></li>
+                        </ul>
+                    </header>
 
-                        <Switch>
-                            <Route exact path="/"
-                                render={(routeProps) => <Dashboard {...routeProps} profiles={profiles} modalHandler={this.modalHandler} />} />
-                            <Route exact path="/teamname"
-                                render={(routeProps) => <Team {...routeProps} filterTeams={filterTeams} profiles={profiles} modalHandler={this.modalHandler} />} />
-                            <Route exact path="/teams"
-                                render={(routeProps) => <Teams {...routeProps} profiles={profiles} teams={teams} />} />
-                        </Switch>
+                    <Switch>
+                        <Route exact path="/"
+                            render={(routeProps) => <Dashboard {...routeProps} profiles={allEmployees} modalHandler={this.modalHandler} />} />
+                        <Route exact path="/teamname"
+                            render={(routeProps) => <Team {...routeProps} filterTeams={filterTeams} profiles={allEmployees} modalHandler={this.modalHandler} />} />
+                        <Route exact path="/teams"
+                            render={(routeProps) => <Teams {...routeProps} profiles={allEmployees} teams={teams} />} />
+                    </Switch>
 
-                        <Modal open={this.state.modal.open} content={this.state.modal.content} handler={modalHandler} />
-                    </main>
-                </div>
-            </BrowserRouter>
-        );
-    }
+                    <Modal open={this.state.modal.open} content={this.state.modal.content} handler={modalHandler} />
+                </main>
+            </div>
+        </BrowserRouter>
+    );
+}
 }
 
 export default App;
